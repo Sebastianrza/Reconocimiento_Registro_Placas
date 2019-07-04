252e7920f34a75dcd31e2183f7546e36657ee1d8
Sebastianrza <45112547+Sebastianrza@users.noreply.github.com>
2019-07-04 16:37:51 -0500

    Add files via upload
    Upload Project

---
+++ RECONOCIMIENTO_PLACAS/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "Reconocimiento_Placas",
+    packages = ["Package"],
+    version = "0.1",
+    description = "Este paquete nos permite reconocer y registrar placas",
+    author = "Sebastian Zambrano",
+    author_email = "Sebastianrza@gmail.com",
+    url = "",
+    keyword =["Placas","automovil","Reconocimiento"]
+)
